f558f07cd3bb5aca95a3b2200121f22089c3f1c7
Luis Adha <40796508+luisadha@users.noreply.github.com>
2023-03-17 14:24:19 +0700

    Add files via upload

---
+++ buat-password-expr-14day.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+randomnumber="$$"
+
+echo 'Alat membuat 12 Karakter untuk Kata-sandi'
+echo ' '
+echo -e "Cara Kerja:\n Kata-sandi akan disimpan di situs transfer.sh selama 14 hari. \nPerhatikan bahwa anda harus mengingat link dibawah agar dapat mengakses Kata-sandi anda yang baru saja dibuat. Dan Tolong jangan Beritahu/bagikan link ini ke Siapapun!."
+echo
+for ((n=0;n<12;n++))
+do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-12
+done | tail -n 1 > $randomnumber.txt
+curl ${STOREDLINK:="--upload-file ./$randomnumber.txt https://transfer.sh/$randomnumber.txt"}
+echo
+echo "Tautan ini berisi Kata-sandi dan akan kadaluarsa selama 14 hari";
+
+rm -f $randomnumber.txt
